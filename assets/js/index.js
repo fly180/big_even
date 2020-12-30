a1073f56a1ab58215551ed8d4669d360779066f2
fly <1787201242@qq.com>
2020-12-30 10:17:19 +0800

    完成功能

---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+  getUserInfo()
+  var layer = layui.layer
+
+  // 点击按钮，实现退出功能
+  $('#btnLogout').on('click', function () {
+    // 提示用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      // 1. 清空本地存储中的 token
+      localStorage.removeItem('token')
+      // 2. 重新跳转到登录页面
+      location.href = '/login.html'
+
+      // 关闭 confirm 询问框
+      layer.close(index)
+    })
+  })
+})
+
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
+    success: function (res) {
+      if (res.status != 0) {
+        return layui.layer.msg('获取失败')
+      }
+      renderAvatar(res.data)
+    },
+    // // 不论成功与失败，最终都会调用complete回调函数
+    // complete: function (res) {
+    //   if (res.responseJSON.satus == 1 && res.responseJSON.message == '身份认证失败！') {
+    //     // 1. 强制清空 token
+    //     localStorage.removeItem('token')
+    //     // 2. 强制跳转到登录页面
+    //     location.href = '/login.html'
+    //   }
+    // }
+  })
+}
+function renderAvatar(user) {
+  let name = user.nickname || user.username
+  $('#welcome').html(`欢迎${name}`)
+  if (user.user_pic != null) {
+    $('.layui-nav-img').attr(src, user.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    $('.layui-nav-img').hide()
+    let first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+  }
+}
